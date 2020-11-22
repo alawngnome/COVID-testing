f2327249cd3f29da2ff5b4461f04c9f3a5d771ec
Kevin Chao <kevinchao2011@gmail.com>
2020-11-22 15:44:00 -0500

    initial login.js page added

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Login from './Components/Login.js';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
   document.getElementById('root')
 );
