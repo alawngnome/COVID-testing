6f2e87079660fd692182f353e8df2a1751b9dc40
sam-ng <ngsamu4@gmail.com>
2020-11-23 20:13:48 -0500

    Added Google Font link in index.js and began implementing some of the UI for WellTesting.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import Login from './Components/Login.js';
 import TestCollection from './Components/TestCollection.js';
 
 import LabHome from './Components/LabHome.js';
+import WellTesting from './Components/WellTesting.js';
 
 function App() {
   //return <TestCollection />;
-  return <LabHome />;
+  //return <LabHome />;
+  return <WellTesting />;
 }
 
 export default App;
