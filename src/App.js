9617ab7e886bce6570174c15c5dba4c5df63bca6
Sadiq <samin.sada@gmail.com>
2020-11-23 23:08:08 -0500

    add edit functionality in PoolMapping

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import Login from './Components/Login.js';
-import EELogin from './Components/EELogin.js';
 import TestCollection from './Components/TestCollection.js';
-
+import WellTesting from './Components/WellTesting';
 import LabHome from './Components/LabHome.js';
 import PoolMapping from './Components/PoolMapping';
-import WellTesting from './Components/WellTesting.js';
 
-export default function App() {
+function App() {
   //return <TestCollection />;
-  //return <LabHome />;
-  //return <WellTesting />;
+  // return <LabHome />;
+  // return <WellTesting />;
   //return <Login />;
   //return <EELogin />;
   return <PoolMapping />;
 }
+
+export default App;
