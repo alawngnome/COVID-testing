397608505b7c8a56f2e44667ec96a644711fce79
Kevin Chao <kevinchao2011@gmail.com>
2020-11-24 16:14:56 -0500

    EEHome loading with table data

--- src/Components/EEHome.js
+++ src/Components/EEHome.js
@@ -52,6 +52,7 @@ export default function EEHome() {
   const classes = useStyles();
 
   const [entries, setEntries] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   let id = '';
   let date = '';
@@ -59,6 +60,7 @@ export default function EEHome() {
 
   useEffect(() => {
     fetchEmployeeHomeData().then((data) => {
+      setLoading(false);
       let document = [];
       for (let i = 0; i < data.testCollection.length; i++) {
         console.log(data.testCollection[i]);
@@ -71,6 +73,9 @@ export default function EEHome() {
     });
   }, []);
 
+  if (loading) {
+    return null;
+  }
   return (
     <div>
       <h2 className={classes.root}>Employee Home</h2>
