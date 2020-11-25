b1c61d19bdfb0f278aaa230941e07199551b3e80
Kevin Chao <kevinchao2011@gmail.com>
2020-11-24 19:14:16 -0500

    i was so scared

--- src/App.js
+++ src/App.js
@@ -22,9 +22,10 @@ function App() {
       } else {
         setUser();
       }
+      unsubscribe();
     });
-    return unsubscribe();
   }, []);
+
   return (
     <div className={classes.container}>
       <BrowserRouter>
